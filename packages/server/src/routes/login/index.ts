6fcfe08b761cd01fd4ac8a2adb551ef85ebcf412
Onur Odemis <onurodemis@gmail.com>
2025-01-05 00:24:26 +0300

    Fix debug and login problem

--- packages/server/src/routes/login/index.ts
+++ packages/server/src/routes/login/index.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import loginController from '../../controllers/login'
 const router = express.Router()
 
-// Login
-router.post('/login', loginController.login)
+router.post('/', loginController.login)
 
 export default router
--- packages/server/src/services/login/index.ts
+++ packages/server/src/services/login/index.ts
@@ -1,17 +1,33 @@
-//import { StatusCodes } from 'http-status-codes'
+import { StatusCodes } from 'http-status-codes'
+import { getRunningExpressApp } from '../../utils/getRunningExpressApp'
 import { InternalFlowiseError } from '../../errors/internalFlowiseError'
 import { getErrorMessage } from '../../errors/utils'
-import { utilLogIn } from '../../utils/login'
+import { User } from '../../database/entities/User'
 
-const login = async (customEmail: string, customPass: string) => {
+const login = async (userEmail: string, encryptPass: string): Promise<User> => {
     try {
-        const dbResponse = await utilLogIn(customEmail, customPass)
-        return dbResponse
+        const appServer = getRunningExpressApp()
+
+        if (typeof !userEmail || !encryptPass) {
+            throw new InternalFlowiseError(
+                StatusCodes.PRECONDITION_FAILED,
+                `Error: loginService.login - User email and password not provided!`
+            )
+        }
+
+        const dbResponse = await appServer.AppDataSource.getRepository(User).find({
+            where: {
+                userEmail
+            }
+        })
+
+        if (!dbResponse || dbResponse.length === 0) {
+            throw new InternalFlowiseError(StatusCodes.NOT_FOUND, `Login ${userEmail} not found`)
+        }
+
+        return dbResponse[0]
     } catch (error) {
-        // throw new InternalFlowiseError(
-        //     StatusCodes.INTERNAL_SERVER_ERROR,
-        //     `Error: loginService.login - ${getErrorMessage(error)}`
-        // )
+        throw new InternalFlowiseError(StatusCodes.INTERNAL_SERVER_ERROR, `Error: loginService.login - ${getErrorMessage(error)}`)
     }
 }
 
